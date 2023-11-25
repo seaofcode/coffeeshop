062d145d2b6f1d0994603a632e5d0690e43981eb
web-mjp <seaofcode@gmail.com>
2023-11-25 17:18:17 -0600

    start fetch function for items

--- src/fetchProducts.js
+++ src/fetchProducts.js
@@ -1,5 +1,9 @@
 import { allProductsUrl } from './utils.js';
 
-const fetchProducts = async () => {};
+const fetchProducts = async () => {
+  const response = await fetch(allProductsUrl).catch((err) => console.log(err));
+  console.log(response);
+  return response;
+};
 
 export default fetchProducts;
