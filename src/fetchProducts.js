121eb7814f4349b30fbf993ae80f7ccf59c09f54
Matthew J Pearson <seaofcode@gmail.com>
2023-11-27 13:01:12 -0600

    fetch js start
    async function to json

--- src/fetchProducts.js
+++ src/fetchProducts.js
@@ -2,7 +2,9 @@ import { allProductsUrl } from './utils.js';
 
 const fetchProducts = async () => {
   const response = await fetch(allProductsUrl).catch((err) => console.log(err));
-  console.log(response);
+  if (response) {
+    return response.json();
+  }
   return response;
 };
 
