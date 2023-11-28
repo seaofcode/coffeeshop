631aca9b37ebdbefa025d9bd1c25d7ced30dde2f
Matthew J Pearson <seaofcode@gmail.com>
2023-11-27 18:23:38 -0600

    starting store script

--- src/store.js
+++ src/store.js
@@ -1,5 +1,10 @@
 import { getStorageItem, setStorageItem } from './utils.js';
 let store = [];
-const setupStore = () => {};
+const setupStore = (products) => {
+    store = products.map((product) => {
+        console.log(product);
+        return product;
+    });
+};
 const findProduct = () => {};
 export { store, setupStore, findProduct };
