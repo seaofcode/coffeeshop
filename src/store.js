32cc0265d13ab9bae634f27e619a489db7ab4f6d
Matthew J Pearson <seaofcode@gmail.com>
2023-11-29 14:25:22 -0600

    setup store js
    finished the store, setting up the API values so they appear side by side when retrieved from the API

--- src/store.js
+++ src/store.js
@@ -2,8 +2,9 @@ import { getStorageItem, setStorageItem } from './utils.js';
 let store = [];
 const setupStore = (products) => {
     store = products.map((product) => {
-        console.log(product);
-        return product;
+        const {id,fields:{featured,name,price,company,colors,image:img }} = product;
+        const image = img[0].thumbnails.large.url;
+        return {id,featured,name,price,company,colors,image};
     });
 };
 const findProduct = () => {};
