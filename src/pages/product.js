a4d8c870f09e258a1778a3d3bfdef8b35af31008
Matthew J Pearson <seaofcode@gmail.com>
2023-11-30 16:26:18 -0600

    single product pg finish
    Grab the id from the API and place the correct fields into the products page using the DOM

--- src/pages/product.js
+++ src/pages/product.js
@@ -28,6 +28,25 @@ window.addEventListener('DOMContentLoaded', async function(){
         const response = await fetch(`${singleProductUrl}${urlID}`);
         if (response.status >= 200 && response.status <= 299) {
             const product = await response.json();
+            // grab data
+            const {id, fields} = product;
+            productID = id;
+            const {name,company,price,colors,description} = fields;
+            const image = fields.image[0].thumbnails.large.url;
+            // set values
+            document.title = `${name.toUpperCase()} | Comfy`;
+            pageTitleDOM.innerHTML = `Home / ${name}`;
+            imgDOM.src = image;
+            titleDOM.textContent = name;
+            companyDOM.textContent = `by ${company}`;
+            priceDOM.textContent = formatPrice(price);
+            descDOM.textContent = description;
+            colors.forEach((color)=>{
+                const span = document.createElement('span');
+                span.classList.add('product-color');
+                span.style.backgroundColor = `${color}`;
+                colorsDOM.appendChild(span);
+            });
         } else {
             console.log(response.status, response.statusText);
             centerDOM.innerHTML = `
@@ -42,3 +61,7 @@ window.addEventListener('DOMContentLoaded', async function(){
 
     loading.style.display = "none";
 });
+
+cartBtn.addEventListener('click', function(){
+    addToCart(productID);
+})
