1532d6ea71f0fa423f3d600f6b3c62c71231a76e
Matthew J Pearson <seaofcode@gmail.com>
2023-12-06 10:30:53 -0600

    remove items from cart

--- src/cart/setupCart.js
+++ src/cart/setupCart.js
@@ -62,6 +62,10 @@ function displayCartItemsDOM() {
   });
 }
 
+function removeItem(id) {
+  cart = cart.filter((cartItem)=> cartItem.id !== id);
+}
+
 function increaseAmount(id) {
   let newAmount;
   cart = cart.map((cartItem)=>{
@@ -75,7 +79,22 @@ function increaseAmount(id) {
 }
 
 function setupCartFunctionality(){
-
+  cartItemsDOM.addEventListener('click', function(e){
+    const element = e.target;
+    const parent = e.target.parentElement;
+    const id = e.target.dataset.id;
+    const parentID = e.target.parentElement.dataset.id;
+    //remove
+    if (element.classList.contains('cart-item-remove-btn')) {
+      removeItem(id);
+      parent.parentElement.remove();
+    }
+    // increase
+    // decrease
+    displayCartItemCount();
+    displayCartTotal();
+    setStorageItem('cart',cart);
+  })
 }
 
 const init = () => {
